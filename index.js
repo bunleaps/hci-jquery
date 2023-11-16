eb9ec865e959baae5c8e415f5c501a9036bdb664
bunleaps <bunleap.sorn@gmail.com>
2023-11-16 11:34:08 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+// import livereload from "livereload";
+// import connectLivereload from "connect-livereload";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// const liveReloadServer = livereload.createServer();
+// liveReloadServer.watch(path.join(__dirname, "public"));
+// app.use(connectLivereload());
+
+// liveReloadServer.server.once("connection", () => {
+//   setTimeout(() => {
+//     liveReloadServer.refresh("/");
+//   }, 100);
+// });
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+
+app.get("/endless-horse", function (req, res) {
+  res.render("pages/endless-horse");
+});
+
+app.get("/skills", function (req, res) {
+  res.render("pages/skills");
+});
+
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080");
+});
